4cf3ff85f70e0e37c30728cfeaf791e8f79d77d2
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2023-03-27 13:16:20 +0200

    finished adapter pattern

---
+++ AdaptiveCode-agile/CH4/Strategy.swift
@@ -0,0 +1,47 @@
+//
+//  Strategy.swift
+//  AdaptiveCode-agile
+//
+//  Created by magdy khalifa on 27/03/2023.
+//
+
+import Foundation
+
+public protocol IStrategy
+{
+    func  Execute();
+}
+// . . .
+public class ConcreteStrategyA : IStrategy
+{
+    public func  Execute()
+    {
+        print("ConcreteStrategyA.Execute()");
+    }
+}
+// . . .
+public class ConcreteStrategyB : IStrategy
+{
+    public func  Execute()
+    {
+        print("ConcreteStrategyB.Execute()");
+    }
+}
+// . . .
+public class Context{
+    
+    private let  strategyA: IStrategy = ConcreteStrategyA();
+    private let  strategyB: IStrategy = ConcreteStrategyB();
+    private var currentStrategy: IStrategy;
+    
+    public init(){
+        currentStrategy = strategyA;
+    }
+    
+    public func DoSomething(){
+        currentStrategy.Execute()
+        
+        // swap strategy with each call
+        currentStrategy = (currentStrategy == strategyA) ? strategyB : strategyA
+    }
+}
