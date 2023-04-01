b392d79053224094bb13f1197386b503b4500a24
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2023-04-01 14:09:02 +0200

    aaa

--- AdaptiveCode-agile/CH06/Refactoring.swift
+++ AdaptiveCode-agile/CH06/Refactoring.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+
 enum AccountType{
     case Silver
     case Gold
@@ -14,16 +15,23 @@ enum AccountType{
 }
 
 class Account{
-    private let type: AccountType;
-
-    
+    private let type: AccountType
     
     init(type: AccountType){
         self.type = type
     }
+    
     private (set) var Balance: Double = 0.0
     
     private (set) var RewardPoints: Int = 0
+    
+    private let GoldBalanceCostPerPoint: Double = 5.0
+    private let PlatinumBalanceCostPerPoint: Double = 10.0
+    
+    private let SilverTransactionCostPerPoint: Double = 10.0
+    private let GoldTransactionCostPerPoint: Double = 5.0
+    private let PlatinumTransactionCostPerPoint: Double = 2.0
+
     
     func AddTransaction(amount: Double){
         RewardPoints += CalculateRewardPoints(amount);
@@ -34,17 +42,13 @@ class Account{
         var points = 0
         switch(type){
         case AccountType.Silver:
-            points = Int(floor(amount / 10));
-            break;
+            points = Int(floor(amount / SilverTransactionCostPerPoint));
         case AccountType.Gold:
-            points = Int(floor((Balance / 10000 * 5) + (amount / 5)))
-            break;
+            points = Int(floor((Balance / 10000 * GoldBalanceCostPerPoint) + (amount / GoldTransactionCostPerPoint)))
         case AccountType.Platinum:
-            points = Int(ceil((Balance / 10000 * 10) + (amount / 2)))
-            break;
+            points = Int(ceil(((Balance / 10000 * PlatinumBalanceCostPerPoint) + (amount / PlatinumTransactionCostPerPoint))))
         default:
             points = 0;
-            break;
         }
         return max(points, 0);
     }
