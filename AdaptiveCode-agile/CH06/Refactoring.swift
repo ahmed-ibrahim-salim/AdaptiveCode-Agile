cd83f335820fbd1029aa760c59f2d818a10da3c9
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2023-04-02 15:03:57 +0200

    aaa

--- AdaptiveCode-agile/CH06/Refactoring.swift
+++ AdaptiveCode-agile/CH06/Refactoring.swift
@@ -7,12 +7,51 @@
 
 import Foundation
 
+// MARK: Factory
+protocol AccountFactoryProtocol{
+    func createAccount(type: AccountType)->AccountBaseProtocol
+}
+class AccountFactory: AccountFactoryProtocol{
+    // factory Method, creates class based on provided type
+    func createAccount(type: AccountType)->AccountBaseProtocol{
+        var account: AccountBaseProtocol
+        
+        switch type{
+        case AccountType.Silver:
+            account = SilverAccount()
+        case AccountType.Gold:
+            account = GoldAccount()
+        case AccountType.Platinum:
+            account = PlatinumAccount()
+        }
+        return account
+    }
+}
+
+enum AccountType{
+    case Silver
+    case Gold
+    case Platinum
+}
+
 class Account{
     private var accountBaseProtocol: AccountBaseProtocol?
-
+    
     private(set) var Balance: Double = 0.0
     private(set) var RewardPoints: Int = 0
     
+    init(accountFactory: AccountFactoryProtocol) {
+        self.accountFactory = accountFactory
+        
+    }
+    
+    var accountFactory: AccountFactoryProtocol
+    func createAccount(type: AccountType)->AccountBaseProtocol{
+        
+        
+        return accountFactory.createAccount(type: type)
+        
+    }
     
     func AddTransaction(amount: Double){
         RewardPoints += CalculateRewardPoints(amount);
@@ -34,23 +73,24 @@ protocol AccountBaseProtocol{
     func CalculateRewardPoints(_ amount: Double)->Int
 }
 
-class SilverAccount: AccountBaseProtocol{
+// These classes are private
+private class SilverAccount: AccountBaseProtocol{
     private let SilverTransactionCostPerPoint: Double = 10.0
-
+    
     private(set) var Balance: Double = 0.0
     
     private(set) var RewardPoints: Int = 1
-        
+    
     func CalculateRewardPoints(_ amount: Double) -> Int {
         return Int(floor(amount / SilverTransactionCostPerPoint));
     }
 }
 
-class GoldAccount: AccountBaseProtocol{
+private class GoldAccount: AccountBaseProtocol{
     
     private let GoldBalanceCostPerPoint: Double = 5.0
     private let GoldTransactionCostPerPoint: Double = 5.0
-
+    
     private(set) var Balance: Double = 0.0
     
     private(set) var RewardPoints: Int = 1
@@ -60,11 +100,11 @@ class GoldAccount: AccountBaseProtocol{
         return Int(floor((Balance / 10000 * GoldBalanceCostPerPoint) + (amount / GoldTransactionCostPerPoint)))
     }
 }
-class PlatinumAccount: AccountBaseProtocol{
+private class PlatinumAccount: AccountBaseProtocol{
     
     private let PlatinumTransactionCostPerPoint: Double = 2.0
     private let PlatinumBalanceCostPerPoint: Double = 10.0
-
+    
     private(set) var Balance: Double = 0.0
     
     private(set) var RewardPoints: Int = 1
